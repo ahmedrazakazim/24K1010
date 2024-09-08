183be51f2a6f17f7f666d74cc74816f9b12944a7
Ahmed Raza <2004ahmadraza@gmail.com>
2024-09-08 23:05:08 +0500

    Create Question 5.c

---
+++ Lab 3/Question 5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+
+float Principle, interestrate, timeperiod, simpleint;
+    
+printf("Enter Principle amount between Rs.100 and Rs.1,000,000 : ");
+scanf(" %f ", &Principle);
+
+if (Principle < 100 || Principle > 1000000) {
+printf("Out of range");
+  return 0;
+}
+  
+printf("Enter Interest Rate between 5 and 10 :");
+scanf(" %f ", &interestrate);
+
+if (interestrate < 5 || interestrate > 10) {
+printf("Out of range");
+  return 0;
+}
+  
+printf("Enter Time Period between 1 year and 10 years: ");
+scanf(" %f ", &timeperiod);
+
+if (timeperiod < 1 || timeperiod > 10)
+{
+printf("Out of range");
+  return 0;
+}
+  
+  simpleint  = (Principle*interestrate*timeperiod)/100;
+  printf("The Simple Interest is %f",simpleint);
+  
+}
