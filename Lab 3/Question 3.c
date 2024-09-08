11dd5b66dd4767dd02c5fced472e9a247aab5148
Ahmed Raza <2004ahmadraza@gmail.com>
2024-09-08 22:46:02 +0500

    Update Question 3.c

--- Lab 3/Question 3.c
+++ Lab 3/Question 3.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main()  { 
+
+int taxRate, tax, salary;
+printf("Enter Tax rate: ");
+scanf("%d", &taxRate);
+
+printf("\nEnter salary : ");
+scanf("%d", &salary);
+  
+tax = (taxRate/100.0)*salary;
+salary = salary - tax;
+printf("Your tax amount is %d and your remaining salary is %d" , tax, salary);
+
+return 0; }
+
+
 
