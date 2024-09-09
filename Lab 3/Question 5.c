d81e825628d7af58e9d668cb98e849e2c4ff8797
Ahmed Raza <2004ahmadraza@gmail.com>
2024-09-10 00:18:00 +0500

    Update Question 5.c

--- Lab 3/Question 5.c
+++ Lab 3/Question 5.c
@@ -7,7 +7,7 @@ float Principle, interestrate, timeperiod, simpleint;
 printf("Enter Principle amount between Rs.100 and Rs.1,000,000 : ");
 scanf(" %f ", &Principle);
 
-if (Principle < 100 || Principle > 1000000) {
+if ((Principle < 100) && (Principle > 1000000)) {
 printf("Out of range");
   return 0;
 }
@@ -15,7 +15,7 @@ printf("Out of range");
 printf("Enter Interest Rate between 5 and 10 :");
 scanf(" %f ", &interestrate);
 
-if (interestrate < 5 || interestrate > 10) {
+if ((interestrate < 5) && (interestrate > 10)) {
 printf("Out of range");
   return 0;
 }
@@ -23,7 +23,7 @@ printf("Out of range");
 printf("Enter Time Period between 1 year and 10 years: ");
 scanf(" %f ", &timeperiod);
 
-if (timeperiod < 1 || timeperiod > 10)
+if ((timeperiod < 1) && (timeperiod > 10))
 {
 printf("Out of range");
   return 0;
