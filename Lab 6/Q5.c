5db6de7faf0479da6309bb39d9d343827800b2ca
Ahmed Raza <2004ahmadraza@gmail.com>
2024-10-03 14:22:47 +0500

    Create Q5.c

---
+++ Lab 6/Q5.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main() {
+int i;
+ for (i=0; i <6;i++){
+ 	if ((i== 0) ||(i==5) ){
+ 	printf("*****\n");
+ 	}
+ 	else {
+ 		
+ 		printf("*   *\n");
+ 	}
+ 	
+ }
+return 0;
+    
+}
