eb0c1728bf0c253e944d4d2a61583afd1f48d7af
Ahmed Raza <2004ahmadraza@gmail.com>
2024-10-03 14:27:38 +0500

    Update Q1.c

--- Lab 6/Q1.c
+++ Lab 6/Q1.c
@@ -1,2 +1,20 @@
-//The while loop would be best for user input
+//The do-while loop would be best for user input
 
+#include <stdio.h>
+
+int main() {
+int a;
+int x=0;
+do {
+    
+printf("Enter a number: ");
+scanf("%d", &a);
+if (a == 0)
+break;
+    printf("%d + %d = %d\n",a,x,x+a); 
+    x=x+a;
+}
+while (a!=0);
+
+    return 0;
+}
