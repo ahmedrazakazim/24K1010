6ea0e6779224d22821dac65dea46de1b2bff3946
Ahmed Raza <2004ahmadraza@gmail.com>
2024-10-10 14:49:06 +0500

    Create Q2.c

---
+++ Lab 7/Q2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+    char input[100];
+    int i;
+    int A = 0, E = 0, I = 0, O = 0, U = 0;
+        printf("Enter word:\n");
+        scanf("%99s", &input[i]);
+
+    for (i = 0; input[i] != '\0' ; i++) {
+    	if(input[i] =='A'|| input[i] == 'a'){
+    		A++;
+		}
+		if(input[i] =='E'|| input[i] == 'e'){
+    		E++;
+		}
+		if(input[i] =='I'|| input[i] == 'i'){
+    		I++;
+		}
+		if(input[i] =='O'|| input[i] == 'o'){
+    		O++;
+		}
+		if(input[i] =='U' || input[i] == 'u'){
+    		U++;
+		}
+}
+	printf("a = %d e = %d i = %d o = %d u = %d", A, E, I, O, U);
+return 0;
+    }
