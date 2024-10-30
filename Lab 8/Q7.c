d8e19909375f087130f6a4b438134fe81eb4290e
Ahmed Raza <2004ahmadraza@gmail.com>
2024-10-30 23:42:23 +0500

    Create Q7.c

---
+++ Lab 8/Q7.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main()
+{
+    char canvas[105][105] = {};
+    int row = 4, col = 0;
+    int direction = 1;
+    int spaceCount = 1;
+    int cycleCount = 0;
+
+    printf("CALCULATING....\n");
+    while (cycleCount != 3)
+    {
+        printf("%d %d %d\n", row, col, spaceCount);
+        canvas[row][col] = '*';
+
+        if (direction)
+        {
+            row--;
+            col += spaceCount;
+            spaceCount++;
+        }
+        else
+        {
+            row++;
+            col += spaceCount;
+            spaceCount--;
+        }
+        if (row == 0 && direction == 1)
+        {
+            direction = 0;
+            spaceCount--;
+        }
+        else if (row == 4 && direction == 0)
+        {
+            spaceCount = 1;
+            direction = 1;
+            cycleCount++;
+            if (cycleCount != 0)
+            {
+                canvas[row][col] = '*';
+                col += 2;
+                if (cycleCount != 3)
+                    canvas[row][col] = '*';
+            }
+        }
+    }
+
+    printf("GRID:\n");
+    int r = 0;
+    while (r <= 4)
+    {
+        int c = 0;
+        while (c
