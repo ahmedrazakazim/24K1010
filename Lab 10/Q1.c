8c224fbe728988b028f4d0ce31b803fce3880125
Ahmed Raza <2004ahmadraza@gmail.com>
2024-11-20 23:00:30 +0500

    Update Q1.c

--- Lab 10/Q1.c
+++ Lab 10/Q1.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-
-void displayArr(int arr[], int n, int start)
-{
-    if (start >= n)
-        return;
-
-    printf("%d ", arr[start]);
-    displayArr(arr, n, start + 1);
-}
-
-int main()
-{
-    int num[] = {0, 1, 2, 3, 4, 5};
-
-    int size = sizeof(num) / sizeof(num[0]);
-
-    displayArr(num, size, 0);
-
+int rec(int arr [100], int n);
+int main() {
+    int n, i, arr [100];
+    printf("Enter number of elements: ");
+    scanf("%d",&n);
+    for (i=0;i<n;i++) {
+        printf("Enter element %d: ",i+1);
+        scanf("%d", &arr[i]);
+    }
+    rec(arr,n-1);
+   
     return 0;
 }
+int rec(int arr [100], int n) {
+    if (n<0) {
+        return 0; }
+        
+  rec(arr, n-1);
+    printf("%d \n", arr[n]);
+    
+} 
