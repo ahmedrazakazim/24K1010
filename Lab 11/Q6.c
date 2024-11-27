1c80b099e49983aa31295c4e0b642d1d8c1b39fa
Ahmed Raza <2004ahmadraza@gmail.com>
2024-11-27 10:18:32 +0500

    Update Q6.c

--- Lab 11/Q6.c
+++ Lab 11/Q6.c
@@ -16,24 +16,17 @@ int main(){
 	
 	for (int i = 0; i < 3; i++) {
         printf("Enter details for Employee %d:\n", i + 1);
-        
         printf("Field Name: ");
-        scanf("%s", employees.field_name);
-        
+        scanf("%s", employees.field_name); 
         printf("Employee ID: ");
         scanf("%s", employees.id);
-        
         printf("Salary: ");
         scanf("%f", &employees.salary);
-        
         printf("City: ");
         scanf("%s", employees.address.city);
-        
-        printf("Zip Code: ");
+	printf("Zip Code: ");
         scanf("%d", &employees.address.zip_code);
-        
         printf("\n");  
-        
          fwrite(&employees,sizeof(Employee),1,ptr);
     }
     fclose(ptr);
@@ -45,17 +38,13 @@ int main(){
    fread(&tempemp,sizeof(Employee),1,ptr);
      
         printf("Field Name:%s", tempemp.field_name);
-        
         printf("\n");
         printf("Employee ID:%s", tempemp.id);
         printf("\n");
-     
         printf("Salary: %.2f", tempemp.salary);
         printf("\n");
-    
         printf("City: %s", tempemp.address.city);
-        
-       printf("\n");
+        printf("\n");
         printf("Zip Code:%d", tempemp.address.zip_code);
         printf(" \n");
  		printf(" \n");       
